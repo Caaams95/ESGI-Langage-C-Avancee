67734742ffdaa4ef1895acf5fb7b8b7c75051f56
Camille Lacouture <camillelctmlc@gmail.com>
2023-05-30 11:16:30 +0200

    Concatenation OK

--- Concatenation/main.c
+++ Concatenation/main.c
@@ -5,22 +5,23 @@
 # include <stdlib.h>
 # include <string.h>
 
+char *concat(char *chaine1, char *chaine2){
+    strcat(chaine1,chaine2);
+    return chaine1;
+}
 
 int main(){
     printf("=============START=============\n");
 
     char *chaine1 = malloc( sizeof(char)*10 );
     char *chaine2 = malloc( sizeof(char)*10 );
-    char *chaine3= malloc( sizeof(char)*20 );
 
     strcpy(chaine1, "Hello ");
     strcpy(chaine2, "World !");
-//    char *chaine3 = concat(chaine1, chaine2);
 
-    strcpy(chaine3,chaine1);
-    strcat(chaine3,chaine2);
+    char *chaine3 = concat(chaine1, chaine2);
 
-    printf("%s\n", chaine3);
-    printf("==============END==============\n");
+    printf("%s", chaine3);
+    printf("\n==============END==============\n");
 
 }
