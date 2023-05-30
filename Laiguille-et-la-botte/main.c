2cb512e996b6bdbfeaf5699ad82d9219cc21a554
Camille Lacouture <camillelctmlc@gmail.com>
2023-05-30 10:53:45 +0200

    Add Laiguille-et-la-botte

---
+++ Laiguille-et-la-botte/main.c
@@ -0,0 +1,35 @@
+//
+// Created by camil on 30/05/2023.
+//
+# include <stdio.h>
+# include <stdlib.h>
+
+# define SIZE 9
+
+int *get_element(int *liste, int nb){
+//    printf("GO get_element\n");
+    for (int i = 0; i < SIZE; ++i) {
+        //printf("%d\n",*(liste + i));
+        if (*(liste + i) == nb)
+            return  (liste+i);
+    }
+    return 0;
+}
+
+int main(){
+    printf("=============START=============\n");
+
+    int maListe[SIZE] = {12, 42, 53, 634, 25, 66, 77, 82, 59};
+    int *liste = malloc(sizeof(int)*SIZE);
+    //INIT
+    for (int i = 0; i < SIZE; ++i) {
+        *(liste + i) = maListe[i];
+    }
+
+    //Check avant fonction
+    printf("%p\n", liste+8);
+    printf("%p", get_element(liste, 59));
+
+
+    printf("==============END==============\n");
+}
