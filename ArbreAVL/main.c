05b7f1212be62e7483b44c9ccebd8676f22afbad
Caaams95 <camillelctmlc@gmail.com>
2023-06-08 03:10:28 +0200

    Arbre AVL Suppr OK

--- ArbreAVL/main.c
+++ ArbreAVL/main.c
@@ -1,112 +1,97 @@
-//
-// Created by admin on 08/06/2023.
-//
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+typedef struct Node {
+    int value;
     struct Node* leftChild;
     struct Node* rightChild;
-    int value;
     int height;
-};
+} Node;
 
-// Fonction utilitaire pour calculer la hauteur d'un nœud
-int getHeight(struct Node* node) {
+int getHeight(Node* node) {
     if (node == NULL)
-        return 0;
+        return -1;
     return node->height;
 }
 
-// Fonction utilitaire pour trouver le maximum de deux entiers
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-// Fonction pour effectuer une rotation simple à gauche
-struct Node* rotateLeft(struct Node* node) {
-    struct Node* newRoot = node->rightChild;
-    struct Node* subtree = newRoot->leftChild;
-
-    newRoot->leftChild = node;
-    node->rightChild = subtree;
-
-    node->height = max(getHeight(node->leftChild), getHeight(node->rightChild)) + 1;
-    newRoot->height = max(getHeight(newRoot->leftChild), getHeight(newRoot->rightChild)) + 1;
-
-    return newRoot;
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->value = value;
+    newNode->leftChild = NULL;
+    newNode->rightChild = NULL;
+    newNode->height = 0;
+    return newNode;
 }
 
-// Fonction pour effectuer une rotation simple à droite
-struct Node* rotateRight(struct Node* node) {
-    struct Node* newRoot = node->leftChild;
-    struct Node* subtree = newRoot->rightChild;
+void updateHeight(Node* node) {
+    if (node == NULL)
+        return;
+    node->height = 1 + max(getHeight(node->leftChild), getHeight(node->rightChild));
+}
 
+Node* rotateRight(Node* node) {
+    Node* newRoot = node->leftChild;
+    node->leftChild = newRoot->rightChild;
     newRoot->rightChild = node;
-    node->leftChild = subtree;
-
-    node->height = max(getHeight(node->leftChild), getHeight(node->rightChild)) + 1;
-    newRoot->height = max(getHeight(newRoot->leftChild), getHeight(newRoot->rightChild)) + 1;
-
+    updateHeight(node);
+    updateHeight(newRoot);
     return newRoot;
 }
 
-// Fonction pour insérer un nœud dans l'AVL
-struct Node* insert(struct Node* node, int value) {
-    if (node == NULL) {
-        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-        newNode->leftChild = NULL;
-        newNode->rightChild = NULL;
-        newNode->value = value;
-        newNode->height = 1;
-        return newNode;
-    }
+Node* rotateLeft(Node* node) {
+    Node* newRoot = node->rightChild;
+    node->rightChild = newRoot->leftChild;
+    newRoot->leftChild = node;
+    updateHeight(node);
+    updateHeight(newRoot);
+    return newRoot;
+}
 
-    if (value < node->value)
-        node->leftChild = insert(node->leftChild, value);
-    else if (value > node->value)
-        node->rightChild = insert(node->rightChild, value);
-    else
-        return node; // La valeur existe déjà, donc rien à faire
+Node* balance(Node* node) {
+    if (node == NULL)
+        return node;
 
-    node->height = max(getHeight(node->leftChild), getHeight(node->rightChild)) + 1;
+    updateHeight(node);
 
-    // Vérification et rééquilibrage de l'arbre
-    int balance = getHeight(node->leftChild) - getHeight(node->rightChild);
+    int balanceFactor = getHeight(node->leftChild) - getHeight(node->rightChild);
 
-    // Cas de rotation à gauche
-    if (balance > 1 && value < node->leftChild->value)
-        return rotateRight(node);
-
-    // Cas de rotation à droite
-    if (balance < -1 && value > node->rightChild->value)
-        return rotateLeft(node);
-
-    // Cas de rotation gauche-droite
-    if (balance > 1 && value > node->leftChild->value) {
-        node->leftChild = rotateLeft(node->leftChild);
+    if (balanceFactor > 1) {
+        if (getHeight(node->leftChild->leftChild) < getHeight(node->leftChild->rightChild))
+            node->leftChild = rotateLeft(node->leftChild);
         return rotateRight(node);
     }
-
-    // Cas de rotation droite-gauche
-    if (balance < -1 && value < node->rightChild->value) {
-        node->rightChild = rotateRight(node->rightChild);
+    if (balanceFactor < -1) {
+        if (getHeight(node->rightChild->rightChild) < getHeight(node->rightChild->leftChild))
+            node->rightChild = rotateRight(node->rightChild);
         return rotateLeft(node);
     }
 
     return node;
 }
 
-// Fonction pour trouver le nœud avec la plus petite valeur (prédécesseur) dans l'arbre
-struct Node* findMinValueNode(struct Node* node) {
-    struct Node* current = node;
-    while (current->leftChild != NULL)
-        current = current->leftChild;
-    return current;
+Node* insertNode(Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->value)
+        root->leftChild = insertNode(root->leftChild, value);
+    else if (value > root->value)
+        root->rightChild = insertNode(root->rightChild, value);
+
+    return balance(root);
 }
 
-// Fonction pour supprimer un nœud de l'AVL
-struct Node* deleteNode(struct Node* root, int value) {
+Node* getMaxNode(Node* root) {
+    if (root == NULL || root->rightChild == NULL)
+        return root;
+    return getMaxNode(root->rightChild);
+}
+
+Node* deleteNode(Node* root, int value) {
     if (root == NULL)
         return root;
 
@@ -115,87 +100,73 @@ struct Node* deleteNode(struct Node* root, int value) {
     else if (value > root->value)
         root->rightChild = deleteNode(root->rightChild, value);
     else {
-        // Nœud trouvé, effectuer la suppression
-
-        // Cas de nœud avec un seul enfant ou sans enfant
-        if (root->leftChild == NULL) {
-            struct Node* temp = root->rightChild;
-            free(root);
-            return temp;
+        if (root->leftChild == NULL || root->rightChild == NULL) {
+            Node* temp = root->leftChild ? root->leftChild : root->rightChild;
+            if (temp == NULL) {
+                temp = root;
+                root = NULL;
+            } else {
+                *root = *temp;
+                free(temp);
+            }
+        } else {
+            Node* maxNode = getMaxNode(root->leftChild);
+            root->value = maxNode->value;
+            root->leftChild = deleteNode(root->leftChild, maxNode->value);
         }
-        else if (root->rightChild == NULL) {
-            struct Node* temp = root->leftChild;
-            free(root);
-            return temp;
-        }
-
-        // Cas de nœud avec deux enfants, trouver le prédécesseur et le remplacer
-        struct Node* minValueNode = findMinValueNode(root->rightChild);
-        root->value = minValueNode->value;
-        root->rightChild = deleteNode(root->rightChild, minValueNode->value);
     }
 
-    root->height = max(getHeight(root->leftChild), getHeight(root->rightChild)) + 1;
-
-    // Vérification et rééquilibrage de l'arbre
-    int balance = getHeight(root->leftChild) - getHeight(root->rightChild);
-
-    // Cas de rotation à gauche
-    if (balance > 1 && getHeight(root->leftChild->leftChild) >= getHeight(root->leftChild->rightChild))
-        return rotateRight(root);
-
-    // Cas de rotation à droite
-    if (balance < -1 && getHeight(root->rightChild->rightChild) >= getHeight(root->rightChild->leftChild))
-        return rotateLeft(root);
-
-    // Cas de rotation gauche-droite
-    if (balance > 1 && getHeight(root->leftChild->leftChild) < getHeight(root->leftChild->rightChild)) {
-        root->leftChild = rotateLeft(root->leftChild);
-        return rotateRight(root);
-    }
-
-    // Cas de rotation droite-gauche
-    if (balance < -1 && getHeight(root->rightChild->rightChild) < getHeight(root->rightChild->leftChild)) {
-        root->rightChild = rotateRight(root->rightChild);
-        return rotateLeft(root);
-    }
-
-    return root;
+    return balance(root);
 }
 
+void inOrderTraversal(Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->leftChild);
 
-// Fonction pour afficher l'arbre AVL (parcours infixe)
-void printAVL(struct Node* node) {
-    if (node == NULL)
-        return;
+        printf("Noeud %d (h%d)", root->value, root->height);
 
-    printAVL(node->leftChild);
-    printf("%d ", node->value);
-    printAVL(node->rightChild);
+        if (root->leftChild != NULL)
+            printf(" | FG: %d", root->leftChild->value);
+        if (root->rightChild != NULL)
+            printf(" | FD: %d", root->rightChild->value);
+
+        printf("\n");
+
+        inOrderTraversal(root->rightChild);
+    }
 }
 
 
 int main() {
-    struct Node* root = NULL;
+    Node* root = NULL;
 
-    // Insertion de quelques valeurs pour tester
-    root = insert(root, 10);
-    root = insert(root, 20);
-    root = insert(root, 30);
-    root = insert(root, 40);
-    root = insert(root, 50);
-    root = insert(root, 60);
+    // Création de l'arbre AVL avec les valeurs et hauteurs spécifiées
+    root = createNode(6);
+    root->height = 3;
+    root->leftChild = createNode(4);
+    root->leftChild->height = 2;
+    root->rightChild = createNode(7);
+    root->rightChild->height = 1;
+    root->leftChild->leftChild = createNode(2);
+    root->leftChild->leftChild->height = 1;
+    root->leftChild->rightChild = createNode(5);
+    root->leftChild->rightChild->height = 0;
+    root->rightChild->rightChild = createNode(8);
+    root->rightChild->rightChild->height = 0;
+    root->leftChild->leftChild->leftChild = createNode(1);
+    root->leftChild->leftChild->leftChild->height = 0;
+    root->leftChild->leftChild->rightChild = createNode(3);
+    root->leftChild->leftChild->rightChild->height = 0;
 
-    printf("Arbre AVL avant suppression : ");
-    printAVL(root);
+    printf("Arbre AVL apres insertion:\n");
+    inOrderTraversal(root);
     printf("\n");
 
-    // Suppression d'une valeur
-    int valueToDelete = 30;
-    root = deleteNode(root, valueToDelete);
+    int nodeToDelete = 5;
+    root = deleteNode(root, nodeToDelete);
 
-    printf("Arbre AVL apres suppression de la valeur %d : ", valueToDelete);
-    printAVL(root);
+    printf("Arbre AVL apres suppression du noeud %d:\n", nodeToDelete);
+    inOrderTraversal(root);
     printf("\n");
 
     return 0;
